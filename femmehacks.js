00d7013140c1bd88662914a00e78cb350279c580
Natalie <nwiegand1@hwemail.com>
2017-02-25 13:15:33 -0500

    js alert

--- femmehacks.js
+++ femmehacks.js
@@ -5,5 +5,23 @@ function process (form)
 	if (drugname = advil)
 	{
 		alert("Take 1-2 advil every 6 hours until symptoms end");
-
-}
+	}
+};
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/*( "form" ).submit(function( event ) {
+  if ( $( "input:first" ).val() === "correct" ) {
+    $( "span" ).text( "Validated..." ).show();
+    return;
+  }
+ 
+  $( "span" ).text( "Not valid!" ).show().fadeOut( 1000 );
+  event.preventDefault();*/
