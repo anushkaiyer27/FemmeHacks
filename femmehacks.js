d0a0c363c2a9e1c363fd27fb3d85f3853e9cee7e
Natalie <nwiegand1@hwemail.com>
2017-02-25 15:19:36 -0500

    js

--- femmehacks.js
+++ femmehacks.js
@@ -7,16 +7,24 @@ function process (form)
 	
 	if (drugname = "advil")
 	{
+		var timeshr = 6;
+		var times day = 0;
+		var side-ef = 
 		alert("Take 1-2 advil every 6 hours until symptoms end");
 	}
 	else if (drugname = "benadryl")
 	{
 		alert ("Take 1 per day until symptoms end");
 	}
-	
+	else if 
+	{
+		
+	}
 	mapLookup(zipcode);
 	
 }
+
+var output = "Take "+name+" every "+ time +". Side effects include: "+ sideeff;
 
 
 function mapLookup(zipcode)
