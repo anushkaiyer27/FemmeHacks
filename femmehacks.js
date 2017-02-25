c7a28ee6c39fb5026f98f59ef5a385df27465111
Natalie <nwiegand1@hwemail.com>
2017-02-25 14:21:27 -0500

    js

--- femmehacks.js
+++ femmehacks.js
@@ -1,13 +1,49 @@
 function process (form)
 {
-	alert("got here!");
+
 	var drugname = form.drugname.value;
+	var zipcode = form.zipcode.value;
+	var phone-number = form.phone-number.value;
 	
-	if (drugname = advil)
+	if (drugname = "advil")
 	{
 		alert("Take 1-2 advil every 6 hours until symptoms end");
 	}
-};
+	else if (drugname = "benadryl")
+	{
+		alert ("Take 1 per day until symptoms end");
+	}
+	
+	mapLookup(zipcode);
+	
+}
+
+
+function mapLookup(zipcode)
+{
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "website", false);
+	
+	
+	xhr.setRequestHeader('Content-Type', 'text/xml');
+xhr.send();
+
+xmlDocument = xhr.responseXML;
+console.log(xmlDocument.childNodes['0'].textContent);
+	
+	GET
+	
+	POST
+	
+	
+	
+	}
+
+
+function nameLookup (drugname)
+{
+	
+	}
 	
 	
 	
