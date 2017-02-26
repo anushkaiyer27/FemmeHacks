51242e13a6ef685eb00acb8198a7d07bc431f63e
Annie Su <anniesumail@gmail.com>
2017-02-25 20:23:17 -0500

    scroll up

--- femmehacks.js
+++ femmehacks.js
@@ -136,6 +136,22 @@ function search() {
     jQuery.getJSON("http://maps.googleapis.com/maps/api/geocode/json?address=" + zip, function(data){
         initialize(data.results[0].geometry.location);
     });
+
+
+/* button */
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
 };
 
 
